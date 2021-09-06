ac9cbf95ffa40114284b9fdd65dc8c795261ecd7
hyemin <hmjang28@gmail.com>
2021-09-06 22:29:00 +0900

    [Add] getAllRecipe method

--- src/service/Service.java
+++ src/service/Service.java
@@ -1,12 +1,25 @@
 package service;
 
+import java.util.ArrayList;
+
+import exception.NotExistException;
+import model.DAO.RecipeDAO;
+import model.DTO.RecipeDTO;
+
 public class Service {
 	private static Service instance = new Service();
+	
+	private static RecipeDAO getRecipeDAO = RecipeDAO.getInstance();
 
 	public Service(){
 	}
 
 	public static Service getInstance(){
 		return instance;
+	}
+
+	// 모든 레시피 조회
+	public ArrayList<RecipeDTO> getAllRecipe() throws NotExistException {
+		return getRecipeDAO.getAllRecipe();
 	}
 }
