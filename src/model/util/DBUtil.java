11a813a58637e4c71134f14b018a215a9d2c2873
hyemin <hmjang28@gmail.com>
2021-09-06 12:33:33 +0900

    [Feature]

---
+++ src/model/util/DBUtil.java
@@ -0,0 +1,23 @@
+package model.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+private static EntityManagerFactory emf;
+	
+	static {
+		emf = Persistence.createEntityManagerFactory("oracleDB");
+	}
+	
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	public static void close() {
+		emf.close();		
+		emf = null;
+	}
+
+}
